37a55924bc2ad773b98619c356e31cf1df3b5f59
Robin Schnaidt <rs@robinschnaidt.com>
2020-06-06 12:19:50 +0200

    Add video animation scripts for 5 and 10 FPS

---
+++ trends-germany/bundeslaender/createAnimVideo10FPS.sh
@@ -0,0 +1,9 @@
+# Crop images
+convert output/*.png -crop 850x686+50x1 output/cropped.png
+
+# Create video
+ffmpeg -framerate 10 -pattern_type glob -i 'output/cropped-*.png' \
+  -c:v libx264 -r 10 -pix_fmt yuv420p output/tempsAnim.mp4
+
+# Delete cropped images
+rm output/cropped-*
